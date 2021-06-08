b4b9fbc13cdb79f479febea3950e44b5168c38a8
uhjee <oper1490@gmail.com>
2021-06-08 18:11:42 +0900

    [javascript] promise 개념 정리

--- javascript/promise/src/index.js
+++ javascript/promise/src/index.js
@@ -3,36 +3,62 @@ const func1 = () =>
   new Promise((resolve, reject) => {
     setTimeout(() => {
       console.log('1')
-      resolve('ho')
+      resolve(true)
       // resolve()
     }, 2000)
   })
 
-const func2 = () =>
-  new Promise((resolve, reject) => {
+const func2 = (res) => {
+  console.log(res)
+
+  return new Promise((resolve, reject) => {
     setTimeout(() => {
       console.log('2')
       resolve()
     }, 3000)
   })
+}
 
 const func3 = () => {
   console.log('3')
 }
 
 // 실행부
-// func1().then(func2).then(func3)
+func1().then(func2).then(func3)
 
-func1()
-  .then((res) => {
-    setTimeout(() => {
-      console.log(res)
-      // throw new Error('1')
-    }, 3000)
-  })
-  .then(() => {
-    return 'fafafa'
-  })
-  .then((argu) => {
-    console.log(argu)
-  })
+// func1()
+//   .then((res) => {
+//     setTimeout(() => {
+//       console.log(res)
+//       // throw new Error('1')
+//     }, 3000)
+//   })
+//   .then(() => {
+//     return 'fafafa'
+//   })
+//   .then((argu) => {
+//     console.log(argu)
+//   })
+
+// const callAPI1 = () =>
+//   new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       console.log('api1 response')
+//     }, 3000)
+//     resolve('api1 response _ resolve')
+//   })
+
+// callAPI1()
+//   .then((response) => {
+//     console.log(response)
+//     setTimeout(() => {
+//       console.log('api2 response')
+//     }, 3000)
+//     return response
+//   })
+//   .then((response) => {
+//     console.log(response)
+//     setTimeout(() => {
+//       console.log('mine! 2')
+//     }, 3000)
+//   })
