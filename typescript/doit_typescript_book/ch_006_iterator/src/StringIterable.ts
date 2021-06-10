20de417aca1aece0511a639599df250c1622d0a3
uhjee <oper1490@gmail.com>
2021-06-10 15:40:47 +0900

    [typescript] doit ch_006 iterator, interface Iterator<T>, Iterable<T>

---
+++ typescript/doit_typescript_book/ch_006_iterator/src/StringIterable.ts
@@ -0,0 +1,18 @@
+export class StringIterable implements Iterable<string> {
+  constructor(private strings: string[] = [], private currentIndex: number = 0) {}
+
+  [Symbol.iterator](): Iterator<string> {
+    const self = this
+    let currentIndex = self.currentIndex,
+      length = self.strings.length
+
+    const iterator: Iterator<string> = {
+      next(): { value: string | undefined; done: boolean } {
+        const value = currentIndex < length ? self.strings[currentIndex++] : undefined
+        const done = value === undefined
+        return { value, done }
+      },
+    }
+    return iterator
+  }
+}
