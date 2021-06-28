4337f2a8c42e38b3f9939ded81a478f962f1cb74
uhjee <oper1490@gmail.com>
2021-06-28 18:11:44 +0900

    [vue/typescript] vue3 + typescript 프로젝트 구성   - 이 프로젝트를 기반으로 vue3+typescript template 구현

---
+++ vue/vue3-typescript/README.md
@@ -0,0 +1,25 @@
+# vue3-typescript
+
+## vue cli를 통해 vue3 + typescript 프로젝트 구성
+
+1. vue cli 설치
+    ```
+    > npm i -g @vue/cli
+    ```
+
+1. vue project 생성
+    ```
+    vue create vue3-typescript
+    ```
+1. 이후 설정에서 Manually select features 선택 
+입맛에 따라 선택
+    - vue3
+    - babel
+    - typescript
+    - css pre-processors
+
+1. 이후 option 선택
+    - vue version : 3.x
+    - Pick a CSS pre-processor : dart-sass 선택
+    - Use class-style component syntax? : NO
+    -  Pick a linter / formatter config:  ESLint + Airbnb config
---
+++ vue/vue3-typescript/src/App.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="row">
+    <div class="col">
+      <div class="row">
+        <button @click="appendNum">appendNum</button>
+      </div>
+      <div class="row" v-for="(item, index) in arr" :key="item + index">{{ item }}</div>
+    </div>
+    <div class="col">
+      <div class="row">
+        <input v-model="message" @input="changeMessage($event.target.value)" type="text" />
+        <button @click="appendTodo">appendTodo</button>
+      </div>
+      <div class="row" v-for="(todo, index) in todoArr" :key="todo + index">{{ todo }}</div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {},
+  data() {
+    return {
+      message: '' as string,
+      arr: [0, 1] as number[],
+      todoArr: ['d', 'dsfa'] as string[],
+      lastNum: 0 as number,
+    };
+  },
+  methods: {
+    appendNum(): void {
+      this.arr.push((this.lastNum += 1));
+    },
+    appendTodo() {
+      if (this.message !== '') {
+        this.todoArr.push(this.message);
+        this.message = '';
+      }
+    },
+    changeMessage(value: string): void {
+      this.message = value.trim();
+    },
+  },
+});
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+
+  .row {
+    display: flex;
+    align-items: center;
+
+    .col {
+      margin: 10px 15px;
+      width: 100%;
+      border-right: 1px solid #888;
+
+      &:last-child {
+        border-right: none;
+      }
+
+      .row {
+        margin: 5px 0;
+      }
+    }
+  }
+}
+</style>
