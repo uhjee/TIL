e25d9419a19728ab423d789902cff77c22887c26
uhjee <oper1490@gmail.com>
2021-07-25 14:30:17 +0900

    [javascript] promise - async, await

---
+++ javascript/promise/src/async_await.js
@@ -0,0 +1,30 @@
+const a = () => new Promise((resolve, reject) => {
+  setTimeout(() => {
+    console.log('A');
+    resolve('resolve A');
+  }, 1000);
+})
+const b = (str) => new Promise((resolve, reject) => {
+  setTimeout(() => {
+    console.log('B');
+    resolve(str + ' resolve B');
+  }, 1000);
+})
+const c = (str) => new Promise((resolve, reject) => {
+  setTimeout(() => {
+    console.log('C');
+    resolve(str + ' resolve C');
+  }, 1000);
+})
+
+const test = async () => {
+  const Ares = await a();
+  console.log(Ares);
+  const Bres = await b(Ares);
+  console.log(Bres);
+  const Cres = await c(Bres);
+  console.log(Cres);
+
+}
+
+test();
