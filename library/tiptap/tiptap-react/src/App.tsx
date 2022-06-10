c2b0c07ad87ccf7614448ab7ad47c842d211d1a9
uhjee <oper1490@gmail.com>
2022-06-11 00:11:04 +0900

    tiptap

--- library/tiptap/tiptap-react/src/App.tsx
+++ library/tiptap/tiptap-react/src/App.tsx
@@ -1,12 +1,13 @@
+import { Reset } from 'styled-reset';
 import React from 'react';
-import './App.css';
 import TextEditor from 'components/TextEditor';
 
 function App() {
   return (
-    <div className="App">
+    <React.Fragment>
+      <Reset />
       <TextEditor />
-    </div>
+    </React.Fragment>
   );
 }
 
