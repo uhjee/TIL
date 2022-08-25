759884a35f4f35b1f279226653d5f403b050725d
uhjee <oper1490@gmail.com>
2022-08-25 17:59:44 +0900

    promise test

--- typescript/typescript_jest/src/index.ts
+++ typescript/typescript_jest/src/index.ts
@@ -1,3 +1,13 @@
 export const sum = (a: number, b: number): number => a + b;
 
-export const range = (from: number, to: number): number[] => (from < to ? [from, ...range(from + 1, to)] : []);
+export const range = (from: number, to: number): number[] =>
+  from < to ? [from, ...range(from + 1, to)] : [];
+
+export const promiseTest = (value: string | null) =>
+  new Promise((resolve, reject) => {
+    if (value === null) reject('fail');
+
+    setTimeout(() => {
+      resolve(value);
+    }, 3000);
+  });
--- typescript/typescript_jest/test/test.ts
+++ typescript/typescript_jest/test/test.ts
@@ -1,11 +1,31 @@
-import { sum, range } from '../src/index';
+import { sum, range, promiseTest } from '../src/index';
 
-test('[sum] 1 + 2 to equal 3?', () => {
-  // toBe() : primitive type을 비교할 때 사용
-  expect(sum(1, 2)).toBe(3);
-});
+describe('각종 함수를 테스트합니다.', () => {
+  describe('sum()', () => {
+    it('number 파라미터를 전달하면, number 타입을 반환해야 함 ', () => {
+      // toBe() : primitive type을 비교할 때 사용
+      expect(sum(1, 2)).toBe(3);
+    });
+  });
 
-test('[range] from 1 to 5 equal [1,2,3,4,5]?', () => {
-  // toStrictEqual() : 같은 타입, 같은 구조의 object를 비교할 때 사용
-  expect(range(1, 5 + 1)).toStrictEqual([1, 2, 3, 4, 5]);
+  describe('range()', () => {
+    it('param1이 param2 보다 작은 숫자로 입력하면, 정상적으로 동작해야 함', () => {
+      // toStrictEqual() : 같은 타입, 같은 구조의 object를 비교할 때 사용
+      expect(range(1, 5 + 1)).toStrictEqual([1, 2, 3, 4, 5]);
+    });
+
+    it('param1이 param2보다 큰 경우, 빈 배열을 반환해야 함', () => {
+      expect(range(10, 1)).toStrictEqual([]);
+    });
+  });
+
+  describe('promiseTest(), Promise 테스트', () => {
+    it('파라미터로 null을 보내면 reject 상태가 되고, \'fail\'을 반환해야 함', async () => {
+      expect(promiseTest(null)).rejects.toMatch('fail');
+    });
+
+    it('파라미터로 string을 보내면 string을 반환해야 함', async () => {
+      expect(promiseTest('happy')).resolves.toBe('happy');
+    });
+  });
 });
