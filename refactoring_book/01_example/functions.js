d9c55ebbe1f8282d37ae9a83f301e92a3e72eb15
uhjee <oper1490@gmail.com>
2023-01-04 09:26:21 +0900

    refactoring chapter01 - 렌더링 함수 추출

--- refactoring_book/01_example/functions.js
+++ refactoring_book/01_example/functions.js
@@ -1,80 +1,86 @@
 const { plays } = require('./datas');
 
-const amountFor = (aPerformance) => {
-  let result = 0;
-
-  switch (playFor(aPerformance).type) {
-    case 'tragedy': // 비극
-      result = 40000;
-      if (aPerformance.audience > 30) {
-        result += 1000 * (aPerformance.audience - 30);
-      }
-      break;
-    case 'comedy': // 희극
-      result = 30000;
-      if (aPerformance.audience > 20) {
-        result += 10000 + 500 * (aPerformance.audience - 20);
-      }
-      result += 300 * aPerformance.audience;
-      break;
-
-    default:
-      throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`);
-  }
-  return result;
-};
-
-const playFor = (aPerformance) => {
-  return plays[aPerformance.playID];
-};
-
-const usd = (aNumber) =>
-  new Intl.NumberFormat('es-US', {
-    style: 'currency',
-    currency: 'USD',
-    minimumFractionDigits: 2,
-  }).format(aNumber / 100);
-
-const volumeCreditsFor = (aPerformance) => {
-  let result = 0;
-  // 포인트 적립
-  result += Math.max(aPerformance.audience - 30, 0);
-  if ('comedy' === playFor(aPerformance).type)
-    // 희극 관객 5명 마다 추가 포인트 제공
-    result += Math.floor(aPerformance.audience / 5);
-
-  return result;
-};
-
-const totalVolumeCredits = (invoice) => {
-  let result = 0;
-  for (let perf of invoice.performances) {
-    result += volumeCreditsFor(perf);
-  }
-  return result;
-};
-
-const totalAmount = (invoice) => {
-  let result = 0;
-  for (let perf of invoice.performances) {
-    result += amountFor(perf);
-  }
-  return result;
-};
-
 const statement = (invoice, plays) => {
-  let result = `청구 내역 (고객명: ${invoice.customer}\n`;
+  const statementData = {}; // 중간 구조 데이터
+  return renderPlainText(statementData, invoice, plays);
 
-  for (let perf of invoice.performances) {
-    // 청구 내역 출력
-    result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${
-      perf.audience
-    }석)\n`;
+  function renderPlainText(data, invoice, plays) {
+    let result = `청구 내역 (고객명: ${invoice.customer}\n`;
+
+    for (let perf of invoice.performances) {
+      // 청구 내역 출력
+      result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${
+        perf.audience
+      }석)\n`;
+    }
+
+    result += `총액: ${usd(totalAmount(invoice))}\n`;
+    result += `적립 포인트: ${totalVolumeCredits(invoice)}점\n`;
+    return result;
   }
 
-  result += `총액: ${usd(totalAmount(invoice))}\n`;
-  result += `적립 포인트: ${totalVolumeCredits(invoice)}점\n`;
-  return result;
+  function amountFor(aPerformance) {
+    let result = 0;
+
+    switch (playFor(aPerformance).type) {
+      case 'tragedy': // 비극
+        result = 40000;
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30);
+        }
+        break;
+      case 'comedy': // 희극
+        result = 30000;
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20);
+        }
+        result += 300 * aPerformance.audience;
+        break;
+
+      default:
+        throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`);
+    }
+    return result;
+  }
+
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID];
+  }
+
+  function usd(aNumber) {
+    return new Intl.NumberFormat('es-US', {
+      style: 'currency',
+      currency: 'USD',
+      minimumFractionDigits: 2,
+    }).format(aNumber / 100);
+  }
+
+  function volumeCreditsFor(aPerformance) {
+    let result = 0;
+    // 포인트 적립
+    result += Math.max(aPerformance.audience - 30, 0);
+    if ('comedy' === playFor(aPerformance).type)
+      // 희극 관객 5명 마다 추가 포인트 제공
+      result += Math.floor(aPerformance.audience / 5);
+
+    return result;
+  }
+
+  function totalVolumeCredits(invoice) {
+    let result = 0;
+    for (let perf of invoice.performances) {
+      result += volumeCreditsFor(perf);
+    }
+    return result;
+  }
+
+  function totalAmount(invoice) {
+    let result = 0;
+    for (let perf of invoice.performances) {
+      result += amountFor(perf);
+    }
+    return result;
+  }
 };
 
 module.exports = {
