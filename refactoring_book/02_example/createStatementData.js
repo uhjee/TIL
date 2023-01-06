479fc4eee53199d8f09c97c8df3dcf2bc2fc4c78
uhjee <oper1490@gmail.com>
2023-01-06 13:41:44 +0900

    refactoring chapter01

--- refactoring_book/02_example/createStatementData.js
+++ refactoring_book/02_example/createStatementData.js
@@ -1,69 +1,54 @@
 export function createStatementData(invoice, plays) {
-  const statementData = {}; // 중간 구조 데이터
-  statementData.customer = invoice.customer;
-  statementData.performances = invoice.performances.map(enrichPerformance);
-  statementData.totalAmount = totalAmount(statementData);
-  statementData.totalVolumeCredits = totalVolumeCredits(statementData);
-  return statementData;
+  const result = {}; // 중간 구조 데이터
+  result.customer = invoice.customer;
+  result.performances = invoice.performances.map(enrichPerformance);
+  result.totalAmount = totalAmount(result);
+  result.totalVolumeCredits = totalVolumeCredits(result);
+  return result;
 
   function enrichPerformance(aPerformance) {
-    const caculator = new PerformanceCaculator(
+    const caculator = createPerformanceCalculator(
       aPerformance,
       playFor(aPerformance),
     );
 
     const result = Object.assign({}, aPerformance); // 얕은 복사
     result.play = caculator.play;
-    result.amount = amountFor(result);
-    result.volumeCredits = volumeCreditsFor(result);
+    result.amount = caculator.amount;
+    result.volumeCredits = caculator.volumeCredits;
     return result;
   }
 
   function playFor(aPerformance) {
     return plays[aPerformance.playID];
   }
+  function totalVolumeCredits(data) {
+    return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
+  }
 
-  function amountFor(aPerformance) {
-    let result = 0;
-
-    switch (aPerformance.play.type) {
-      case 'tragedy': // 비극
-        result = 40000;
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30);
-        }
-        break;
-      case 'comedy': // 희극
-        result = 30000;
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 20);
-        }
-        result += 300 * aPerformance.audience;
-        break;
-
-      default:
-        throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
-    }
-    return result;
+  function totalAmount(data) {
+    return data.performances.reduce((total, p) => total + p.amount, 0);
   }
 }
-function volumeCreditsFor(aPerformance) {
-  let result = 0;
-  // 포인트 적립
-  result += Math.max(aPerformance.audience - 30, 0);
-  if ('comedy' === aPerformance.play.type)
-    // 희극 관객 5명 마다 추가 포인트 제공
-    result += Math.floor(aPerformance.audience / 5);
 
-  return result;
-}
-
-function totalVolumeCredits(data) {
-  return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
-}
-
-function totalAmount(data) {
-  return data.performances.reduce((total, p) => total + p.amount, 0);
+/**
+ * 생성자 역할을 하는 팩토리 함수
+ * play.type에 따른 분기처리 로직을 다형성으로 해결하기 위해 class로 각각 반환한다.
+ *
+ * @param   {[type]}  aPerformance  [aPerformance description]
+ * @param   {[type]}  aPlay         [aPlay description]
+ *
+ * @return  {[type]}                [return description]
+ */
+function createPerformanceCalculator(aPerformance, aPlay) {
+  switch (aPlay.type) {
+    case 'tragedy':
+      return new TragedyCalculator(aPerformance, aPlay);
+    case 'comedy':
+      return new ComedyCalculator(aPerformance, aPlay);
+    default:
+      throw new Error(`알 수 없는 장르: ${aPlay.type}`);
+  }
 }
 
 /**
@@ -73,5 +58,39 @@ class PerformanceCaculator {
   constructor(aPerformance, aPlay) {
     this.performance = aPerformance;
     this.play = aPlay;
+  }
+
+  get amount() {
+    throw new Error('오류 발생, 서브 클래스에서 처리하세요.');
+  }
+
+  get volumeCredits() {
+    return Math.max(this.performance.audience - 30, 0);
+  }
+}
+
+class TragedyCalculator extends PerformanceCaculator {
+  get amount() {
+    let result = 40000;
+    if (this.performance.audience > 30) {
+      result += 1000 * (this.performance.audience - 30);
+    }
+    return result;
+  }
+}
+
+class ComedyCalculator extends PerformanceCaculator {
+  get amount() {
+    let result = 30000;
+    if (this.performance.audience > 20) {
+      result += 10000 + 500 * (this.performance.audience - 20);
+    }
+    result += 300 * this.performance.audience;
+    return result;
+  }
+
+  get volumeCredits() {
+    // 희극 관객 5명 마다 추가 포인트 제공
+    return super.volumeCredits + Math.floor(this.performance.audience / 5);
   }
 }
