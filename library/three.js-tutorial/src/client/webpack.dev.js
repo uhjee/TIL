468f63217802e4e2d7809a3fca489fafd29830c8
uhjee <oper1490@gmail.com>
2024-03-07 18:40:36 +0900

    animation loop

--- library/three.js-tutorial/src/client/webpack.dev.js
+++ library/three.js-tutorial/src/client/webpack.dev.js
@@ -1,14 +1,15 @@
-const {merge} = require('webpack-merge')
-const common = require('./webpack.common.js')
-const path = require('path')
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+const path = require('path');
 
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'eval-source-map',
   devServer: {
+    port: 5555,
     static: {
       directory: path.join(__dirname, '../../dist/client'),
     },
     hot: true,
   },
-})
+});
