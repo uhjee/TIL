bafadff3b223eed198adef2cd50c0cc33f5f8cb1
uhjee <oper1490@gmail.com>
2024-03-11 23:40:53 +0900

    three.js tutorial - dat gui

---
+++ library/three.js-tutorial/readme.md
@@ -0,0 +1,26 @@
+# Three.js
+
+- Boilerplate
+    ```shell
+    git clone https://github.com/Sean-Bradley/Three.js-TypeScript-Boilerplate.git
+    cd Three.js-TypeScript-Boilerplate
+    npm install
+    npm run dev
+    ```
+
+- Boilerplate `statsgui`
+    ```shell
+    git clone https://github.com/Sean-Bradley/Three.js-TypeScript-Boilerplate.git
+    cd Three.js-TypeScript-Boilerplate
+    git checkout statsgui
+    npm install
+    npm run dev
+    ```
+
+## 1. Scene, Camera, Render
+
+## 2. Animation Loop
+
+## 3. Development
+    - Stats Panel
+    - Dat GUI
--- library/three.js-tutorial/src/client/client.ts
+++ library/three.js-tutorial/src/client/client.ts
@@ -1,6 +1,7 @@
 import * as THREE from 'three';
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
 import Stats from 'three/examples/jsm/libs/stats.module';
+import {GUI} from 'dat.gui';
 
 const scene = new THREE.Scene();
 
@@ -37,23 +38,30 @@ function onWindowResize() {
   render();
 }
 
-// function animate() {
-//   // 재귀 호출을 통해 1초에 최대 60회까지 호출
-//   window.requestAnimationFrame(animate);
-//
-//   cube.rotation.x += 0.01;
-//   cube.rotation.y += 0.01;
-//
-//   render();
-// }
 
 // 렌더링 관련 통계 데이터 확인 가능 development
 const stats = new Stats();
 document.body.appendChild(stats.dom);
 
+// [Dat.GUI 라이브러리] user interface로 scene 및 object 조작 가능
+const gui = new GUI();
+const cubeFolder = gui.addFolder('Cube')
+cubeFolder.add(cube.rotation, 'x', 0, Math.PI * 2);
+cubeFolder.add(cube.rotation, 'y', 0, Math.PI * 2);
+cubeFolder.add(cube.rotation, 'z', 0, Math.PI * 2);
+cubeFolder.open()
+const cameraFolder = gui.addFolder("Camera Folder");
+cameraFolder.add(camera.position, 'z', 0, 20)
+cameraFolder.open();
+
 function animate() {
-  requestAnimationFrame(animate);
-  stats.update();
+  // 재귀 호출을 통해 1초에 최대 60회까지 호출
+  window.requestAnimationFrame(animate);
+
+  // cube.rotation.x += 0.01;
+  // cube.rotation.y += 0.01;
+
+  stats.update(); // 통계 자료 element update
   render();
 }
 
