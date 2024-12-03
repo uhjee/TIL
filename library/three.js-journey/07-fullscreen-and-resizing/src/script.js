c0b2905f431486bd4091197528849397f490eb65
uhjee <oper1490@gmail.com>
2024-12-04 00:25:07 +0900

    Three.js... - Fullscreen, Viewport, Resizing

---
+++ library/three.js-journey/07-fullscreen-and-resizing/src/script.js
@@ -0,0 +1,85 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
+/**
+ * Base
+ */
+// Canvas
+const canvas = document.querySelector('canvas.webgl');
+
+// Scene
+const scene = new THREE.Scene();
+
+/**
+ * Object
+ */
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+/**
+ * Sizes
+ */
+const sizes = {
+  width: window.innerWidth, // viewport width
+  height: window.innerHeight, // viewport height
+};
+
+window.addEventListener('resize', () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height; // Camera 생성자의 두 번째 인자에 전달되는 값
+  camera.updateProjectionMatrix(); // 카메라의 투영 행렬을 업데이트하여 비율 변경 반영
+  
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+});
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100,
+);
+camera.position.z = 3;
+scene.add(camera);
+
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+// renderer.setSize() 호출 시, Three.js가 canvas의 크기를 조정하고 렌더링 영역을 설정
+renderer.setSize(sizes.width, sizes.height);
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock();
+
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+
+  // Update controls
+  controls.update();
+
+  // Render
+  renderer.render(scene, camera);
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
+
+tick();
