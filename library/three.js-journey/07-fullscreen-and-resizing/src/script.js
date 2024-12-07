543ed4679f31cb18771d32550662ca4596c9ac51
uhjee <oper1490@gmail.com>
2024-12-08 00:18:29 +0900

    Three.js journey   - fullscreen interaction   - handle pixel ratio

--- library/three.js-journey/07-fullscreen-and-resizing/src/script.js
+++ library/three.js-journey/07-fullscreen-and-resizing/src/script.js
@@ -22,10 +22,30 @@ scene.add(mesh);
  * Sizes
  */
 const sizes = {
-  width: window.innerWidth, // viewport width
-  height: window.innerHeight, // viewport height
+  width: window.innerWidth, // viewport width For responsive design
+  height: window.innerHeight, // viewport height For responsive design
 };
 
+window.addEventListener('dblclick', () => {
+  const fullscreenElement =
+    document.fullscreenElement || document.webkitFullscreenElement;
+  if (!fullscreenElement) {
+    if (canvas.requestFullscreen) {
+      canvas.requestFullscreen(); // requestFullscreen()은 HTML 요소를 전체 화면 모드로 표시하는 메서드
+      // 사용자 제스처(클릭, 키보드 입력 등)에 의해 호출되어야 하며, Promise를 반환
+    } else if (canvas.webkitRequestFullscreen) {
+      canvas.webkitRequestFullscreen(); // webkitRequestFullscreen()은 WebKit 브라우저에서 전체 화면 모드로 표시하는 메서드
+    }
+  } else {
+    if (document.exitFullscreen) {
+      document.exitFullscreen(); // Document 인터페이스의 메서드로 전체화면 모드를 종료
+    } else if (document.webkitExitFullscreen) {
+      document.webkitExitFullscreen(); // webkitExitFullscreen()은 WebKit 브라우저에서 전체 화면 모드를 종료하는 메서드
+    }
+  }
+});
+
+// ! Resize event listener
 window.addEventListener('resize', () => {
   // Update sizes
   sizes.width = window.innerWidth;
@@ -34,9 +54,10 @@ window.addEventListener('resize', () => {
   // Update camera
   camera.aspect = sizes.width / sizes.height; // Camera 생성자의 두 번째 인자에 전달되는 값
   camera.updateProjectionMatrix(); // 카메라의 투영 행렬을 업데이트하여 비율 변경 반영
-  
+
   // Update renderer
-  renderer.setSize(sizes.width, sizes.height);
+  renderer.setSize(sizes.width, sizes.height); // 렌더러의 크기를 조정하여 렌더링 영역 설정
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2)); // 디바이스의 픽셀 비율을 고려하여 렌더링 품질 조절
 });
 /**
  * Camera
