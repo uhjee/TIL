93f96dd6e6bf62afebfb7414efb4d1e5969324ed
uhjee <oper1490@gmail.com>
2024-12-22 16:19:37 +0900

    Three.js jouney - lil-gui - 색상 컨트롤 - Geometry 설정

--- library/three.js-journey/09-debug-ui/src/script.js
+++ library/three.js-journey/09-debug-ui/src/script.js
@@ -4,6 +4,24 @@ import gsap from 'gsap';
 import GUI from 'lil-gui';
 
 /**
+ * !Debug
+ */
+const gui = new GUI({
+  width: 300,
+  title: 'Nice Debug UI',
+  // closeFolders: true, // 폴더 닫기 기능 활성화
+});
+gui.hide(); // 디버깅 패널 숨기기
+
+window.addEventListener('keydown', (event) => {
+  if (event.key === 'h') {
+    gui.show(gui._hidden);
+  }
+});
+
+const debugObject = {};
+
+/**
  * Base
  */
 // Canvas
@@ -15,22 +33,22 @@ const scene = new THREE.Scene();
 /**
  * Object
  */
+debugObject.color = '#a778d8';
+
 const geometry = new THREE.BoxGeometry(1, 1, 1, 2, 2, 2);
 const material = new THREE.MeshBasicMaterial({
-  color: '#ff0000',
+  color: debugObject.color,
+  wireframe: true,
 });
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
-/**
- * !Debug
- */
-const gui = new GUI();
+const cubeTweaks = gui.addFolder('Cube Tweaks');
 
-// mesh의 y축 위치를 조정하는 GUI 컨트롤러
+// !mesh의 y축 위치를 조정하는 GUI 컨트롤러
 // min: -3 (최소값), max: 3 (최대값), step: 0.001 (조정 단위)
 // name: elevation (GUI에 표시될 이름)
-gui.add(mesh.position, 'y').min(-3).max(3).step(0.001).name('elevation');
+cubeTweaks.add(mesh.position, 'y').min(-3).max(3).step(0.001).name('elevation');
 
 // // 사용자 정의 변수 조정
 // // GUI 컨트롤을 위한 객체 생성
@@ -40,25 +58,59 @@ gui.add(mesh.position, 'y').min(-3).max(3).step(0.001).name('elevation');
 // };
 // gui.add(myObject, 'myVariable').min(0).max(1000).step(1).name('myVariable');
 
-// mesh의 visible 속성을 조정하는 GUI 컨트롤러
-gui.add(mesh, 'visible');
-gui.add(material, 'wireframe');
+// !mesh의 visible 속성을 조정하는 GUI 컨트롤러
+cubeTweaks.add(mesh, 'visible');
+cubeTweaks.add(material, 'wireframe');
 
-// // 컬러
-// // gui.addColor(material, 'color');
-// gui.addColor({ color: '#ff0000' }, 'color').onChange(value => {
+// !컬러
+// cubeTweaks.addColor(material, 'color');
+// cubeTweaks.addColor(material, 'color').onChange((value) => {
 //   console.log(material.color.getHexString());
 //   console.log(value.getHexString());
+//   // console.log(material.color instanceof THREE.Color);
+//   // console.log(value instanceof THREE.Color);
+
 //   // material.color.set(value); // 선택한 색상을 material.color에 적용
 //   // material.color.convertSRGBToLinear(); // sRGB 색상으로 변환
 // });
-gui
-    .addColor(material, 'color')
-    .onChange((value) => {
-        console.log(value.getHexString()) // 내부적으로 사용되는 HEX 값 출력
-    })
 
+// // 색상 불일치 문제 확인용 - 내부적으로 사용되는 HEX 값 출력값과 디버깅 패널 컬러와 불일치 확인
+// gui.addColor(material, 'color').onChange((value) => {
+//   console.log(value.getHexString()); // 내부적으로 사용되는 HEX 값 출력
+// });
 
+// 외부 변수를 통해 색상 불일치 문제 해결
+cubeTweaks.addColor(debugObject, 'color').onChange((value) => {
+  material.color.set(debugObject.color);
+  console.log(material.color.getHexString());
+});
+
+// ! 버튼을 통한 함수 호출
+debugObject.spin = () => {
+  gsap.to(mesh.rotation, { y: mesh.rotation.y + Math.PI * 2 });
+};
+cubeTweaks.add(debugObject, 'spin');
+
+// !Geometry 세분화 조정
+debugObject.subdivision = 2;
+cubeTweaks
+  .add(debugObject, 'subdivision')
+  .min(1)
+  .max(10)
+  .step(1)
+  .name('subdivisions')
+  .onFinishChange(() => {
+    mesh.geometry.dispose(); // 기존 기하 객체 해제 (GPU 메모리에서 해제)
+
+    mesh.geometry = new THREE.BoxGeometry(
+      1,
+      1,
+      1,
+      debugObject.subdivision,
+      debugObject.subdivision,
+      debugObject.subdivision,
+    );
+  });
 
 /**
  * Sizes
